d39248d0d1646dff9f0b629170cab77664ccf584
owl352 <kokosinca123@gmail.com>
2025-03-07 23:37:48 +0300

    update

--- clients/platform/v0/nodejs/PlatformPromiseClient.js
+++ clients/platform/v0/nodejs/PlatformPromiseClient.js
@@ -66,8 +66,6 @@ const {
             GetIdentityBalanceResponse: PBJSGetIdentityBalanceResponse,
             GetContestedResourceVoteStateRequest: PBJSGetContestedResourceVoteStateRequest,
             GetContestedResourceVoteStateResponse: PBJSGetContestedResourceVoteStateResponse,
-            GetContestedResourcesRequest: PBJSGetContestedResourcesRequest,
-            GetContestedResourcesResponse: PBJSGetContestedResourcesResponse,
           },
         },
       },
@@ -96,7 +94,6 @@ const {
   GetStatusResponse: ProtocGetStatusResponse,
   GetIdentityBalanceResponse: ProtocGetIdentityBalanceResponse,
   GetContestedResourceVoteStateResponse: ProtocGetContestedResourceVoteStateResponse,
-  GetContestedResourcesResponse: ProtocGetContestedResourcesResponse,
 } = require('./platform_protoc');
 
 const getPlatformDefinition = require('../../../../lib/getPlatformDefinition');
@@ -197,14 +194,6 @@ class PlatformPromiseClient {
 
     this.client.getIdentityBalance = promisify(
       this.client.getIdentityBalance.bind(this.client),
-    );
-
-    this.client.getContestedResourceVoteState = promisify(
-      this.client.getContestedResourceVoteState.bind(this.client),
-    );
-
-    this.client.getContestedResources = promisify(
-      this.client.getContestedResources
     );
 
     this.protocolVersion = undefined;
@@ -833,35 +822,6 @@ class PlatformPromiseClient {
             ),
             protobufToJsonFactory(
               PBJSGetContestedResourceVoteStateRequest,
-            ),
-          ),
-        ],
-        ...options,
-      },
-    );
-  }
-
-  getContestedResources(
-    getContestedResourcesRequest,
-    metadata = {},
-    options = {},
-  ) {
-    if (!isObject(metadata)) {
-      throw new Error('metadata must be an object');
-    }
-
-    return this.client.getContestedResources(
-      getContestedResourcesRequest,
-      convertObjectToMetadata(metadata),
-      {
-        interceptors: [
-          jsonToProtobufInterceptorFactory(
-            jsonToProtobufFactory(
-              ProtocGetContestedResourcesResponse,
-              PBJSGetContestedResourcesResponse,
-            ),
-            protobufToJsonFactory(
-              PBJSGetContestedResourcesRequest,
             ),
           ),
         ],
